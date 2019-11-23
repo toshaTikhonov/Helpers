c07f46a64ec5c451c3187fc7b10fbc62773da777
tosha <tosha@armshine.net>
2019-11-23 12:41:28 +0300

    добавлена функция h_BigEndianToUINT реализующая преобразование из big-endian в 4х байтовое целое

--- h_bin.c
+++ h_bin.c
@@ -1,1 +1,18 @@
 #include "h_bin.h"
+
+/**
+  @brief преобразование бинарных данных в формате big-endian в четырех байтовое целое
+  @param p_buf - бинарных данных в формате big-endian
+  @return 4-х байтовое целое
+*/
+unsigned int  h_BigEndianToUINT( unsigned char* p_buf )
+{
+    unsigned int res_uint;
+    
+    res_uint  = (unsigned int)(p_buf[0]); res_uint <<= 8;
+    res_uint |= (unsigned int)(p_buf[1]); res_uint <<= 8;
+    res_uint |= (unsigned int)(p_buf[2]); res_uint <<= 8;
+    res_uint |= (unsigned int)(p_buf[3]);
+
+    return res_uint;
+} /* h_BigEndianToUINT */
--- h_bin.h
+++ h_bin.h
@@ -5,6 +5,13 @@
 extern "C" {
 #endif /* __cplusplus */
 
+/**
+  @brief преобразование бинарных данных в формате big-endian в четырех байтовое целое
+  @param p_buf - бинарных данных в формате big-endian
+  @return 4-х байтовое целое
+*/
+unsigned int  h_BigEndianToUINT( unsigned char* p_buf );
+
 
 #ifdef __cplusplus
 }
