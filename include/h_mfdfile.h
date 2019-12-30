38e02eff68c66ed1b37888663bc37feee7c39ff4
tosha <tosha@armshine.net>
2019-12-30 23:13:28 +0300

    добавлено структуры данных для mifare classic

--- include/h_mfdfile.h
+++ include/h_mfdfile.h
@@ -5,7 +5,50 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#pragma pack(1)
 
+/*
+* @brief  MIFARE Classic
+*/
+typedef struct {
+  unsigned char  abtUID[4];  // beware for 7bytes UID it goes over next fields
+  unsigned char  btBCC;
+  unsigned char  btSAK;      // beware it's not always exactly SAK
+  unsigned char  abtATQA[2];
+  unsigned char  abtManufacturer[8];
+} h_mf_classic_block_manufacturer;
+
+/*
+* @brief  MIFARE Classic data block
+*/
+typedef struct {
+  unsigned char  abtData[16];
+} h_mf_classic_block_data;
+
+/*
+* @brief  MIFARE Classic block trailer
+*/
+typedef struct {
+  unsigned char  abtKeyA[6];
+  unsigned char  abtAccessBits[4];
+  unsigned char  abtKeyB[6];
+} h_mf_classic_block_trailer;
+/*
+* @brief  MIFARE Classic sector
+*/
+typedef union {
+  h_mf_classic_block_manufacturer mbm;
+  h_mf_classic_block_data mbd;
+  h_mf_classic_block_trailer mbt;
+} h_mf_classic_block;
+/*
+* @brief  MIFARE Classic card structure
+*/
+typedef struct {
+  h_mf_classic_block amb[256];
+} h_mf_classic_tag;
+
+#pragma pack()
 
 
 #ifdef __cplusplus
